74ba3c6d793e7e6b9dccf368920e7d68b41224d2
LeandroaBatioja <batiojamena12@gmail.com>
2025-12-17 08:33:05 -0500

    FRONTED V2.2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -57,7 +57,7 @@ export default function Page() {
       {/* BOTÓN DE CERRAR SESIÓN */}
       <button
         onClick={logout}
-        className="fixed top-4 right-4 px-4 py-2 bg-red-500 text-white rounded-xl hover:bg-red-600 transition"
+        className="top-4 right-4 px-4 py-2 bg-red-500 text-white rounded-xl hover:bg-red-600 transition"
       >
         Cerrar Sesión ({user.rol})
       </button>
